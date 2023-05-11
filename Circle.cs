9fa1b3b17e289f44cb6e11a7971beb6563509a06
robolev <92596597+robolev@users.noreply.github.com>
2023-05-11 19:34:53 +0200

    SetFunctions

--- Circle.cs
+++ Circle.cs
@@ -2,9 +2,22 @@
 {
     using SFML.Graphics;
     using SFML.System;
-    using SFML.Window;
-    internal class Circle
+    class Circle
     {
+        CircleShape circle = new CircleShape(50);
+
+        public void SetSizesOfCircle()
+        {
+            circle.Origin = new Vector2f(50, 50);
+        }
+        public void SetPositionOfCircle(int x,int y)
+        {
+            circle.Position += new Vector2f(x, y);
+        }
+        public void SetColour()
+        {
+            circle.FillColor = Color.Red;
+        }
     }
 }
 
